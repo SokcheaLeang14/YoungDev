c96168dcf8a3650ae04de3caf89902eb726d04a2
virakkhun <rakmanoban@gmail.com>
2022-05-27 11:18:01 +0700

    modified: app-services-provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    { 
+	if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+	}
        
     }
 }
