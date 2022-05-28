03f107c67b53c2d03411bfb030f84dd552ed6a7d
virakkhun <rakmanoban@gmail.com>
2022-05-28 21:35:24 +0700

    rollback

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use App\Http\Controllers\api\StudentsController;
 |
 */
 
-// Route::post('/students/register', [StudentsController::class, 'register']);
+Route::post('/students/register', [StudentsController::class, 'register']);
 
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::post('/students/login', [StudentsController::class, 'login']);
-// });
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/students/login', [StudentsController::class, 'login']);
+});
