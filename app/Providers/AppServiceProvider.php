088d3a5a61289868b34e4cd1a4751ae116748aa2
virakkhun <rakmanoban@gmail.com>
2022-05-28 20:32:29 +0700

    fixed: api endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    { 
-	if ($this->app->environment('production')) {
+    {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
-	}
-       
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\StudentsController;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
